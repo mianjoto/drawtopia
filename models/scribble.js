bcfdb6f1b0f04118d2d73d48132030a8070f4459
James <lsuholloway@gmail.com>
2023-02-17 16:28:13 -0500

    Update scribble.js

--- models/scribble.js
+++ models/scribble.js
@@ -3,14 +3,24 @@ const Sequelize = require('sequelize')
 const sequelize = require('../config/database')
   
 const Scribble = sequelize.define('scribble', {
+    scribble_id:{
+        type:Sequelize.INTEGER,
+
+        autoIncrement:true,
+
+        allowNull:false,
+
+        primaryKey:true
+    },
     image: {
-        type: Sequelize.BLOB,
+        type: Sequelize.BLOB('medium'),
         allowNull: false
     },
     author_name: {
         type: Sequelize.STRING,
         allowNull: false
     },
+
     time_posted: {
         type: Sequelize.DATE,
         defaultValue: Sequelize.NOW
