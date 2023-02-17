439bbb72e1c0697ca8b101b50f9bf7b03eab8e6a
mianjoto <mianjoto@gmail.com>
2023-02-17 15:53:26 -0500

    Adding chatroom routing and router

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 
 app.set("view engine", "ejs")
 
-
-app.get('/', (req, res) => {
+app.get('/welcome', (req, res) => {
     res.render("welcome")
 })
+
+const chatroomRouter = require('./chatroom')
+app.use('/', chatroomRouter)
+
 
 app.listen(3000)
