8c2f552e23be0c72d0d120b566a1bb594bf470f6
Rin Nguyen <rnguye15@uncc.edu>
2023-02-17 22:47:13 -0500

    Added undo function. Fixed bug where pen would still be down after mouse moveoutside canvas. Added touchscreen compatibility

--- canvas.js
+++ canvas.js
@@ -3,80 +3,129 @@ const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
 const imageLink = document.createElement('a');
 const byteSize = str => new Blob([str]).size;
-const offsetx = 5;
-const offsety = 54;
+
 
 canvas.width = 1250;
 canvas.height = 350;
 
+let color = "black";
 let isPainting = false;
 let lineWidth = 5;
-let initialX;
-let initialY;
-ctx.fillStyle = "#ffffff";
+
+let undo_array = [];
+let index = -1;
+
+
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas.addEventListener("mousedown", start);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stop);
+canvas.addEventListener("mouseout", stop);
+
+canvas.addEventListener("touchstart", start); 
+canvas.addEventListener("touchmove", draw);
+canvas.addEventListener("touchend", stop);
+
+toolbar.addEventListener("change", changecolor);
+toolbar.addEventListener("click", clickEventHandler);
+
+
+function start(event){
+    isPainting = true;
+    ctx.beginPath()
+    ctx.moveTo(event.clientX - canvas.offsetLeft, 
+               event.clientY - canvas.offsetTop);
+    event.preventDefault();
+}
+
+function draw(event){
+    if(isPainting){
+        ctx.lineTo(event.clientX - canvas.offsetLeft, 
+                   event.clientY - canvas.offsetTop);
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.stroke();
     }
 
-    if (e.target.id === 'size1') {
+    event.preventDefault();
+}
+
+function stop(event){
+    if(isPainting){
+        ctx.stroke();
+        ctx.closePath();
+        isPainting = false;
+    }
+
+    event.preventDefault();
+    if(event.type != "mouseout"){
+        undo_array.push(ctx.getImageData(0, 0, canvas.width, canvas.height))
+        index +=1;
+    }
+}
+
+function changecolor(event){
+    color = event.target.value;
+    ctx.strokeStyle = event.target.value;
+}
+
+function clickEventHandler(event){
+
+    if(event.target.id === "pen"){
+        ctx.strokeStyle = color;
+    }
+
+    if(event.target.id === "size1"){
         lineWidth = 5;
     }
 
-    if (e.target.id === 'size2') {
+    if(event.target.id === "size2"){
         lineWidth = 10;
     }
 
-    if (e.target.id === 'size3') {
+    if(event.target.id === "size3"){
         lineWidth = 20;
     }
 
-    if (e.target.id === 'size4') {
-        lineWidth = 30;
+    if(event.target.id === "size4"){
+        lineWidth = 35;
     }
 
-    if (e.target.id === 'size5') {
+    if(event.target.id === "size5"){
         lineWidth = 50;
     }
 
-    if (e.target.id == 'download'){
-        imageLink.download = 'canvas.jpg'
-        imageLink.href = canvas.toDataURL('image/jpg', 1);
-        imageLink.click();
-        console.log(imageLink.href);
-        console.log(byteSize(imageLink.href));
-    }
-});
-
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
-});
-
-const draw = (e) => {
-    if(!isPainting) {
-        return;
+    if(event.target.id === "eraser"){
+        ctx.strokeStyle = "white"
     }
 
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = 'round';
-    ctx.lineTo(e.clientX - offsetx, e.clientY - offsety);
-    ctx.stroke();
+    if(event.target.id === "undo"){
+        if(index <= 0){
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            undo_array = [];
+            index = -1;
+        }else {
+            index  -= 1;
+            undo_array.pop();
+            ctx.putImageData(undo_array[index], 0, 0);
+        }
+    }
+
+    if(event.target.id === "clear"){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        undo_array = [];
+        index = -1;
+    }
+
+    if(event.target.id === "download"){
+         imageLink.download = 'canvas.jpg'
+         imageLink.href = canvas.toDataURL('image/jpg', 1);
+         imageLink.click();
+         console.log(imageLink.href);
+         console.log(byteSize(imageLink.href));
+    }
 }
-
-canvas.addEventListener('mousedown', e => {
-    isPainting = true;
-    initialX = e.clientX;
-    initialY = e.clientY;
-});
-
-canvas.addEventListener('mouseup', e => {
-    isPainting = false;
-    ctx.stroke();
-    ctx.beginPath();
-});
-
-canvas.addEventListener('mousemove', draw);
