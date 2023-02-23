ffda06a30d4d4e3956043281aa616905c0e72a33
mianjoto <mianjoto@gmail.com>
2023-02-22 21:03:03 -0500

    Adding username validation when user tries to visit root directory without signing in

--- chatroom.js
+++ chatroom.js
@@ -5,6 +5,9 @@ const router = express.Router()
 router.get("/", (req, res) => {
     let username = req.session.username;
     res.render("chatroom", { username })
+    if (!username) {
+        res.redirect('/welcome')
+    }
 })
 
 module.exports = router
