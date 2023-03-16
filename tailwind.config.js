2ff1b8c800f5b7301fdfa14b1d853805389bc779
mianjoto <mianjoto@gmail.com>
2023-03-16 15:15:51 -0400

    Extracting tailwind config from html to root directory config file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+      colors: {
+        'primary-color': '#8077fe',
+        'secondary-color': '#6b63c9',
+        'tertiary-color': '#49467d',
+        'black-blue': '#383651',
+        'dark-grey': '#27242c',
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
